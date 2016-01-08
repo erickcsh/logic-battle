6f68f7892d1e7ee3a56ba7524a1f901d55fd9c2d
Erick Castillo <erickcsh@gmail.com>
2016-01-08 20:44:23 +0100

    Remove scopa logic add android

--- client/play.js
+++
@@ -1,7 +0,0 @@
-Template.hand.events({
-  'click .card': function(evt, template) {
-    if(template.data.yourTurn) {
-      Meteor.call('takeTurn', template.data._id, Meteor.userId(), this);
-    }
-  }
-});

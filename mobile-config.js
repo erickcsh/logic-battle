98d21e051b14b5cefe2ca5d6f1332dd2043ba468
Erick Castillo <erickcsh@gmail.com>
2016-01-26 23:36:43 +0100

    fix mobile settings, avoid users getting to not existing rooms, fix mobile style

--- mobile-config.js
+++ mobile-config.js
@@ -9,11 +9,8 @@ App.icons({
   // iOS
   'iphone': 'resources/logo/ios/Icon-Small-50.png',
   'iphone_2x': 'resources/logo/ios/Icon-Small-50@2x.png',
-  'iphone5': 'resources/logo/ios/Icon.png',
-  'iphone6': 'resources/logo/ios/Icon@2x.png',
-  'iphone6p': 'resources/logo/ios/Icon-60.png',
   'ipad': 'resources/logo/ios/Icon-72.png',
-  'ipad_2x': 'resources/logo/ios/Icon-72@2x.png'
+  'ipad_2x': 'resources/logo/ios/Icon-72@2x.png',
 
   // Android
   'android_ldpi': 'resources/logo/android/mipmap-ldpi/ic_launcher.png',
@@ -24,4 +21,5 @@ App.icons({
 
 App.setPreference('Orientation', 'portrait');
 App.setPreference('SplashScreen', 'CDVSplashScreen')
+App.setPreference('HideKeyboardFormAccessoryBar', true);
 
